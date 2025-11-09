45591b8279cad67f282a5032de38dcf453f11ab2
Katlego Ngakatau <154595367+Kemo-Kat@users.noreply.github.com>
2025-11-09 22:05:46 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
+# relationship_app/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .models import views
 from .views import list_books
+
 app_name = 'relationship_app'
+app_name = 'accounts'
 
 urlpatterns = [
     # Function-based view URL - lists all books
@@ -17,5 +20,24 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('relationship/', include('relationship_app.urls')),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+        # Public URLs
+    path('', views.book_list, name='book_list'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    
+    # Secured URLs with custom permissions
+    path('book/add/', views.book_create, name='book_create'),
+    path('book/<int:pk>/edit/', views.book_update, name='book_update'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    
+    # Management dashboard
+    path('dashboard/', views.book_management_dashboard, name='book_dashboard'),
 
 ]
+
+
+
+
+
