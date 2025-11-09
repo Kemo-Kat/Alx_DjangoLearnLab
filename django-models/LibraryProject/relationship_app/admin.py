3d43ff6f4b23393541b068678d5de953842d5192
Katlego Ngakatau <154595367+Kemo-Kat@users.noreply.github.com>
2025-11-09 19:35:18 +0200

    Update admin.py

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,6 +1,14 @@
 # relationship_app/admin.py
 from django.contrib import admin
 from .models import Author, Book, Library, Librarian
+from django.contrib import admin
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'role']
+    list_filter = ['role']
+    search_fields = ['user__username', 'user__email']
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
