eb8c935fdd97c36baf110be1efdd5f0abc4919ef
Katlego Ngakatau <154595367+Kemo-Kat@users.noreply.github.com>
2025-11-09 18:53:06 +0200

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,19 @@
+# relationship_app/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'relationship_app'
+
+urlpatterns = [
+    # Function-based view URL - lists all books
+    path('books/', views.list_books, name='list_books'),
+    
+    # Class-based view URL - displays specific library details
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    path('admin/', admin.site.urls),
+    
+    path('relationship/', include('relationship_app.urls')),
+
+]
+
