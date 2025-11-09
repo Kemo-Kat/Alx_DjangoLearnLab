af1cd247d1b66752bffa3d44348c088203e481ea
Katlego Ngakatau <154595367+Kemo-Kat@users.noreply.github.com>
2025-11-09 19:10:45 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,39 @@
 # relationship_app/views.py
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Library, Book  # Add Library to import
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from .models import Book, Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all().select_related('author')
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# relationship_app/views.py (continued)
 # Class-based view to display library details
 class LibraryDetailView(DetailView):
-    model = Library  # This requires the Library import
-    template_name = 'relationship_app/library_detail.html'  # Add the template name
-    context_object_name = 'library'  # Add context object name
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
     
     def get_queryset(self):
         return Library.objects.prefetch_related('books__author')
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 
+# Example of a protected view
+@login_required
+def profile(request):
+    return render(request, 'relationship_app/profile.html')
