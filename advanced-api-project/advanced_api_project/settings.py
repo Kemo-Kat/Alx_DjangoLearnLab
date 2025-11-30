8814e7646ef606fbe6fb65169c0ed5c3546a4b92
Katlego Ngakatau <154595367+Kemo-Kat@users.noreply.github.com>
2025-11-30 23:04:49 +0200

    Update settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -106,7 +106,9 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # Default to allow any
+        'rest_framework.permissions.AllowAny',
     ],
 }
